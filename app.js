36eeae60d4af4793f5a97cb17ebffed147930acd
Dorian Buck <dorianbuck@gmail.com>
2021-12-05 13:39:26 +0100

    adds enum functionality

--- app.js
+++ app.js
@@ -2,11 +2,36 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Dorian",
+//   age: 42,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+var ADMIN = 0;
+var READ_ONLY = 1;
+var AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: "Dorian",
     age: 42,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
 };
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [0, "admin"];
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
--- app.ts
+++ app.ts
@@ -2,11 +2,39 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Dorian",
+//   age: 42,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person = {
   name: "Dorian",
   age: 42,
   hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
 };
+
+// person.role.push("admin");
+// person.role[1] = 10;
+
+// person.role = [0, "admin"];
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
@@ -16,4 +44,3 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
-
